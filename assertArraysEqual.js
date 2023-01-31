6fbaa6f23ce951da0c50824a8d96cd63acc0e563
Henry Truong <henry.trb@gmail.com>
2023-01-31 01:33:53 -0500

    added assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,32 @@
+const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    console.log(false);
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      console.log(false);
+      return false;
+    }
+  }
+  console.log(true);
+  return true;
+};
+
+//Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+
+//TEST
+assertArraysEqual([1,2,3], [1,2,3]); // expects pass
+assertArraysEqual([1,2,3], [4,5,6]); // expects fail
+assertArraysEqual([1,2,3], [1]); // expects fail
+assertArraysEqual([1,1,1,1,1], [1,1,1,1,1]); // expects pass
