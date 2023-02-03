68267ddc9ce0fdad12c4a524464ad58674d39307
Henry Truong <henry.trb@gmail.com>
2023-02-02 23:31:57 -0500

    added countLetters function

---
+++ countLetters.js
@@ -0,0 +1,20 @@
+// The function should take in a sentence (as a string) and then return a count of each of the letters in that sentence.
+// For example, countLetters('LHL') should return results indicating that L appears twice, and H once.
+
+
+const countLetters = (str) => {
+  let result = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (result[str[i]]) {
+      result[str[i]] += 1;
+    } else {
+      result[str[i]] = 1;
+    }
+  }
+  return result;
+};
+
+//TEST
+console.log(countLetters("LHL IS COOL"));
+
