d02f506aead0f620ac1c4c0b2c8c215eb5f0f80c
Henry Truong <henry.trb@gmail.com>
2023-02-18 21:24:12 -0500

    refactored middle using mocha and chai

--- middle.js
+++ middle.js
@@ -1,8 +1,4 @@
-const eqArrays = require("./eqArrays");
-const assertArraysEqual = require("./assertArraysEqual");
-
 //Implement middle which will take in an array and return the middle-most element(s) of the given array.
-
 const middle = (array) => {
   let middleArray = [];
   if (array.length < 3) {
@@ -20,4 +16,16 @@ const middle = (array) => {
   }
 };
 
+// assertArraysEqual(middle([1]), []); // pass
+// assertArraysEqual(middle([1, 2]), []); // pass
+
+// //For arrays with odd number of elements, an array containing a single middle element should be returned.
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+
+// //For arrays with an even number of elements, an array containing the two elements in the middle should be returned
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+console.log(middle([1, 2, 3, 4]), [2, 3]);
 module.exports = middle;
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,18 @@
-const assertArraysEqual = require("../assertArraysEqual");
+// const assertArraysEqual = require("../assertArraysEqual");
 const middle = require("../middle");
-// TESTS
+const assert = require("chai").assert;
 
-//For arrays with one or two elements, there is no middle. Return an empty array.
-
-assertArraysEqual(middle([1]), []); // pass
-assertArraysEqual(middle([1, 2]), []); // pass
-
-//For arrays with odd number of elements, an array containing a single middle element should be returned.
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-
-//For arrays with an even number of elements, an array containing the two elements in the middle should be returned
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe("#middleTest", () => {
+  it("returns [] for arrays with 1 elements", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns [] for arrays with 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+  it("returns middle element for arrays with odd # of elements, returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns middle element for arrays with even # of elements, returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+});
