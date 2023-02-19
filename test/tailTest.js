22256ebd0210e1204d6084a4ea2cbac2086d6b22
Henry Truong <henry.trb@gmail.com>
2023-02-18 21:38:38 -0500

    refactored tail test using M&C

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,11 @@
-const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it(`returns eveything minus first element, returns [2,3,4] for [1,2,3,4]`, () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it(`returns eveything minus first element, returns ["2","3","4"] for ["1","2","3","4"]`, () => {
+    assert.deepEqual(tail(["1", "2", "3", "4"]), ["2", "3", "4"]);
+  });
+});
