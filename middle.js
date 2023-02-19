05091f3aaea342e1093b7420d667eb218b4cdcf2
Henry Truong <henry.trb@gmail.com>
2023-02-18 21:47:10 -0500

    removed extra console log

--- middle.js
+++ middle.js
@@ -27,5 +27,4 @@ const middle = (array) => {
 // assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
 // assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 
-console.log(middle([1, 2, 3, 4]), [2, 3]);
 module.exports = middle;
